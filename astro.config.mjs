f685e7f6bfa0af5ad4ae4fd881bec098582dac82
ChristopherMeek <chris@sensemaking.uk.com>
2025-05-30 09:03:20 +0100

    _

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import rehypeMermaid from 'rehype-mermaid'
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid']
+    },
+    rehypePlugins:[[rehypeMermaid, { strategy: 'img-svg', dark: true, colorScheme: 'forest' }]]
+  },
   integrations: [mdx()]
 });
